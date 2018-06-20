5197d9f2b88d1f201538a8a17e271ee68c47cbde
quandtifier <quandtifier@gmail.com>
2018-06-20 10:27:49 -0700

    json present but not rendering in browser

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,5 +1,13 @@
-var myApp = angular.module('myApp',[]);
-myApp.controller('AppCtrl',['$scope','$http',
+var app = angular.module('myApp',[]);
+app.controller('AppCtrl',['$scope','$http',
 function($scope,$http) {
     console.log("Hello from the controller");
 }]);
+
+$http.get('/contactlist').then(function(response) {
+    console.log("I got the data I requested");
+    $scope.contactlist = response;
+    console.log(response);
+},function(error) {
+    console.log("shit went wrong");
+});
--- server.js
+++ server.js
@@ -1,6 +1,31 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.get('/contactlist', function (req, res) {
+    console.log("I recieved a get request");
 
+    person1 = { 
+        name: 'Tim',
+        email: 'tim@email.com',
+        number: '(111) 111-1111'
+    };
+    person2 = { 
+        name: 'Emily',
+        email: 'emily@email.com',
+        number: '(222) 222-2222'
+    };
+    person3 = { 
+        name: 'John',
+        email: 'john@email3.com',
+        number: '(333) 333-3333'
+    };
+
+    var contactlist = [person1,person2,person3];
+
+    res.send(contactlist);
+});
 app.listen(3000);
 console.log("Server running on port 3000");
