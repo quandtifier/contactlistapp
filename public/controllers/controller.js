283007f31f066cf2be37ae4bfd42f0b5bddc2e98
quandtifier <quandtifier@gmail.com>
2018-07-09 17:37:23 -0700

    finished the tutorial through 5 and have the CRUD functionality

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,12 +2,50 @@ var app = angular.module('myApp',[]);
 app.controller('AppCtrl',
 function($scope,$http) {
     console.log("Hello from the controller");
-    $http.get('/contactlist').then(function(response) {
-        console.log("I got the data I requested");
-        $scope.contactlist = response.data;
-        console.log(response);
-    },function(error) {
-        console.log("shit went wrong");
-    });
-});
+    var refresh = function() {
+        $http.get('/contactlist').then(function(response) {
+            console.log("I got the data I requested");
+            $scope.contactlist = response.data;
+            console.log(response);
+        },function(error) {
+            console.log("shit went wrong");
+        });
+    }
 
+    refresh();
+
+    $scope.addContact = function() {
+        console.log($scope.contact);
+        $http.post('/contactlist', $scope.contact).then(function(response) {
+            console.log(response);
+            refresh();
+        });
+    };
+
+    $scope.remove = function(id) {
+        console.log(id);
+        $http.delete('/contactlist/' + id).then(function(response) {
+            refresh();
+        });
+    };
+
+    $scope.edit = function(id) {
+        console.log("id:" + id);
+        $http.get('/contactlist/' + id).then(function(response) {
+            console.log(response);
+            $scope.contact = response.data;
+        });
+    };
+
+    $scope.update = function() {
+        console.log($scope.contact._id);
+        $http.put('/contactlist/' + $scope.contact._id, $scope.contact).then(function(response) {
+            refresh();
+        });
+    };
+
+    $scope.deselect = function() {
+        $scope.contact = "";
+    };
+
+})
--- server.js
+++ server.js
@@ -1,30 +1,70 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 //var bodyParser = require('body-parser');
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 //app.use(bodyParser.json());
 app.get('/contactlist', function (req, res) {
     console.log("I recieved a get request");
 
-    person1 = { 
-        name: 'Tim',
-        email: 'tim@email.com',
-        number: '(111) 111-1111'
-    };
-    person2 = { 
-        name: 'Emily',
-        email: 'emily@email.com',
-        number: '(222) 222-2222'
-    };
-    person3 = { 
-        name: 'John',
-        email: 'john@email3.com',
-        number: '(333) 333-3333'
-    };
+    db.contactlist.find(function (err, docs) {
+        console.log(docs);
+        res.send(docs);
+    });
+    // person1 = { 
+    //     name: 'Tim',
+    //     email: 'tim@email.com',
+    //     number: '(111) 111-1111'
+    // };
+    // person2 = { 
+    //     name: 'Emily',
+    //     email: 'emily@email.com',
+    //     number: '(222) 222-2222'
+    // };
+    // person3 = { 
+    //     name: 'John',
+    //     email: 'john@email3.com',
+    //     number: '(333) 333-3333'
+    // };
 
-    var contactlist = [person1,person2,person3];
+    // var contactlist = [person1,person2,person3];
 
-    res.send(contactlist);
+    // res.send(contactlist);
+});
+app.post('/contactlist', function(req, res) {
+    console.log(req.body);
+    db.contactlist.insert(req.body, function(err, doc) {
+        res.send(doc);
+    })
+});
+
+app.delete('/contactlist/:id', function(req, res) {
+    var id = req.params.id;
+    console.log(id);
+    db.contactlist.remove({_id: mongojs.ObjectId(id)}, function(err, doc) {
+        res.send(doc);
+    })
+});
+
+app.get('/contactlist/:id', function(req, res) {
+    var id = req.params.id;
+    console.log(id);
+    db.contactlist.findOne({_id: mongojs.ObjectId(id)}, function(err, doc) {
+        res.send(doc);
+    });
+});
+
+app.put('/contactlist/:id', function(req, res) {
+    var id = req.params.id;
+    console.log(req.body.name);
+    db.contactlist.findAndModify({query: {_id: mongojs.ObjectId(id)},
+        update: {$set: {name: req.body.name, email: req.body.email, number: req.body.number}},
+        new: true}, function(err,doc) {
+            res.send(doc);
+        });
 });
 app.listen(3000);
 console.log("Server running on port 3000");
